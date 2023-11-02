40a0ae90a40d673629f1affc7d0335b04ca91a1e
Nam Yechan <chanmias0318@gmail.com>
2023-11-02 22:51:27 +0900

    ADD: firebase conf

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB_O3-SWyUlgy7ruWiTh8oWIA91sfO_NYQ",
+  authDomain: "nwitter-re-c8325.firebaseapp.com",
+  projectId: "nwitter-re-c8325",
+  storageBucket: "nwitter-re-c8325.appspot.com",
+  messagingSenderId: "1054676738162",
+  appId: "1:1054676738162:web:615d755a499806539862eb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
